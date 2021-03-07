4b45555b88116b5e58bdb5e03e05956546dbc31f
Anthony <filippea@oregonstate.edu>
2021-03-06 17:43:17 -0800

    Updated Leapyear
    The leap year function now correctly handles inputs that are divisible by 400.

--- LeapYear.py
+++ LeapYear.py
@@ -1,6 +1,8 @@
 def LeapYear(x):
     if(x/4.0 == round(x/4.0)):
         if(x/100.0 == round(x/100.0)):
+            if(x/400.0 == round(x/400.0)):
+                    return True
             return False
         return True
     return False
